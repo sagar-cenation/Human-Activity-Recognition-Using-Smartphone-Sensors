7eaeca81dbec152d16d5fa8603d02ab731d17186
Tilak1996 <tilakpatel@ymail.com>
2017-12-03 13:10:10 +0530

    Got best Accu. 93% on SVM, no PCA

--- svm.py
+++ svm.py
@@ -13,11 +13,27 @@ from sklearn import datasets
 from sklearn.metrics import confusion_matrix as cf
 from sklearn import svm
 
+def classify(train,train_labels,test,test_labels,method,SVMtype):
+    ovoclf = svm.SVC(decision_function_shape=SVMtype)
+    ovoclf.fit(train,train_labels)
+    print("Accuracy in ",method," = ",ovoclf.score(test,test_labels))
+
 train_data = pd.read_csv('datasets/train.csv')
 test_data = pd.read_csv('datasets/test.csv')
 
-x_train = train_data.drop('Activity', axis=1)
-pca = decomposition.PCA(n_components=40)
-x_train_pca = pca.fit_transform(x_train)
+train_pts = train_data.drop('Activity', axis=1)
+train_labels = train_data['Activity']
 
-print(pca.explained_variance_ratio_)
+test_pts = test_data.drop('Activity', axis=1)
+test_labels = test_data['Activity']
+
+
+
+#pca = decomposition.PCA(n_components=40)
+#train_pca = pca.fit_transform(train_pts)
+#print(pca.explained_variance_ratio_)
+#test_pca = pca.fit_transform(test_pts)
+#print(pca.explained_variance_ratio_)
+
+#classify(train_pca,train_labels,test_pca,test_labels,"PCA(ovr)",'ovr')
+classify(train_pts,train_labels,test_pts,test_labels,"Normal",'ovr')
